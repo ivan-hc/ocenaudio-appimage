a2ddaf820136393fd12f330f1f353207cab16f96
iVAN <88724353+ivan-hc@users.noreply.github.com>
2024-08-19 06:41:15 +0200

    Update ocenaudio.sh

--- ocenaudio.sh
+++ ocenaudio.sh
@@ -1,38 +1,48 @@
 #!/bin/sh
 
+APP=ocenaudio
+
+# DOWNLOAD APPIMAGETOOL
+if ! test -f ./appimagetool; then
+	wget -q https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage -O appimagetool
+	chmod a+x ./appimagetool
+fi
+
 # CREATE A TEMPORARY FOLDER
-mkdir tmp
-cd tmp
+mkdir tmp && cd tmp || exit 1
+
 # DOWNLOAD THE OFFICIAL DEB PACKAGE
-wget "https://www.ocenaudio.com$(curl -Ls https://www.ocenaudio.com/download | tr '"' '\n' | grep -i "^/.*deb$" | head -1)"
-ar x ./*.deb
-tar -xf ./*tar.xz
-cd ..
+if ! test -f ./*.deb; then
+	wget "https://www.ocenaudio.com/downloads/index.php/$(curl -Ls https://www.ocenaudio.com/download | tr '/"' '\n' | grep -i "$APP.*deb" | head -1)?" -O "$APP".deb
+	ar x ./*.deb
+	tar xf ./data.tar*
+	tar xf ./control.tar*
+fi
+VERSION=$(cat ./control | grep -i "^version" | cut -c 10-)
+cd .. || exit 1
+
 # CREATE THE APPIMAGE'S DIRECTORY AND ITS STRUCTURE
-mkdir ocenaudio.AppDir
-mv ./tmp/opt/ocenaudio/* ./ocenaudio.AppDir
-cp ./tmp/usr/share/icons/hicolor/128x128/apps/ocenaudio.png ./ocenaudio.AppDir/
-cp ./tmp/usr/share/applications/ocenaudio.desktop ./ocenaudio.AppDir/
+rm -Rf ./"$APP".AppDir/*
+mkdir -p ./"$APP".AppDir
+mv ./tmp/opt/"$APP"/* ./"$APP".AppDir/
+cp ./tmp/usr/share/icons/hicolor/128x128/apps/"$APP".png ./"$APP".AppDir/
+cp ./tmp/usr/share/applications/"$APP".desktop ./"$APP".AppDir/
+
 # THIS IS THE APPRUN SCRIPT NEEDED TO READ AND RUN THE CONTENT OF THE APPIMAGE
-cat >> ./ocenaudio.AppDir/AppRun << 'EOF'
+cat >> ./"$APP".AppDir/AppRun << 'EOF'
 #!/bin/sh
 HERE="$(dirname "$(readlink -f "${0}")")"
 export UNION_PRELOAD="${HERE}"
-export LD_PRELOAD=$(cd / && find . -name "libbz2*" 2>/dev/null | head -1 | cut -c 2-)
-export QT_FONT_DPI=96
-export QT_QPA_PLATFORMTHEME=$QT_QPA_PLATFORMTHEME
 export PATH="${HERE}"/bin/:"${PATH}"
-export LD_LIBRARY_PATH=/lib/:/lib64/:/lib/x86_64-linux-gnu/:/usr/lib/:/usr/lib/x86_64-linux-gnu/:/usr/lib64/:"${HERE}"/lib/:"${LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH="${HERE}"/lib/:"${LD_LIBRARY_PATH}"
 case "$1" in
 	'') exec ${HERE}/bin/ocenaudio;;
 	*) exec ${HERE}/bin/ocenvst "$*";;
 esac
 EOF
-chmod a+x ./ocenaudio.AppDir/AppRun
+chmod a+x ./"$APP".AppDir/AppRun
+
 # DOWNLOAD APPIMAGETOOL AND EXPORT THE DIRECTORY TO AN APPIMAGE
-wget -q https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage -O appimagetool
-chmod a+x ./appimagetool
-ARCH=x86_64 ./appimagetool --comp zstd --mksquashfs-opt -Xcompression-level --mksquashfs-opt 20 ./ocenaudio.AppDir
-version=$(wget -q https://www.ocenaudio.com/fileinfo/ocenaudio_mint64.deb -O - | grep Versão | cut -c 43- | rev | cut -c 5- | rev)
-mv ocenaudio-x86_64.AppImage ocenaudio-$version-x86_64.AppImage
+ARCH=x86_64 ./appimagetool --comp zstd --mksquashfs-opt -Xcompression-level --mksquashfs-opt 20 ./"$APP".AppDir
+mv ./*.AppImage ./"$APP"-"$VERSION"-x86_64.AppImage
 rm -R -f ./tmp
